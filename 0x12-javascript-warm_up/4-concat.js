23f563d369d13c41d5a4a89b3d76f54c2153c2a0
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-11 21:01:02 +0000

    Task 4 completed and uploaded

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* A script that prints two arguments passed to it, in the following format: “ is ” */
+
+const arg1 = process.argv[2]; // first argument
+const arg2 = process.argv[3]; // second argument
+
+console.log(`${arg1} is ${arg2}`);
