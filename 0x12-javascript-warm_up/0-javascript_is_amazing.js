be0d974ef561f587bf7681968fc355675a00d5d4
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-11 19:17:32 +0000

    Task zero completed and uploaded

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* A script that prints a string */
+
+const myVar = 'JavaScript is amazing'; // creating constant variable
+
+console.log(myVar); // function that prints
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,7 @@
+0x12. JavaScript - Warm up
+
+Background Context
+JavaScript is used for many things. Here, i will use JavaScript for 2 reasons:
+
+-Scripting
+-Web front-end
