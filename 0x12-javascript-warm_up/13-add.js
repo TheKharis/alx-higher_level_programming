0e5a1fde6e0376105aa32ac848f734bc545bc1cd
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-11 23:40:19 +0000

    Task 13 completed and uploaded

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* A function that returns the addition of 2 integers
+ * The function must be visible from outside
+ * The name of the function must be add
+ */
+
+exports.add = function add (a, b) {
+  return a + b;
+};
