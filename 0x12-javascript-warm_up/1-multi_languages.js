754e45068bdebf4632263af0c55c5835fb5c4d0c
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-11 19:39:38 +0000

    Task 1 completed and uploaded

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* A script that prints 3 lines of code */
+
+console.log('C is fun'); // First line of code
+console.log('Python is cool'); // Second line of code
+console.log('JavaScript is amazing'); // Third line of code
