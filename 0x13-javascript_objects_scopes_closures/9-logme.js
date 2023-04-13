5c000dcddbb2730396940c81bbaabcc2c034bc3c
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-13 23:48:12 +0000

    Task 9 complete and uploaded

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* a function that prints the number of arguments already printed and the new argument value
+ * Prototype: exports.logMe = function (item)
+ */
+
+let count = 0;
+
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++;
+};
