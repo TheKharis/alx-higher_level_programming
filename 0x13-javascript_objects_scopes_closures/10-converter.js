b5dda275e02b00ae4a75b41ba1fda265563c0f44
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-13 23:55:15 +0000

    Task 7 completed and uploaded

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* a function that converts a number from base 10 to another base passed as argument
+ * Prototype: exports.converter = function (base)
+ */
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
