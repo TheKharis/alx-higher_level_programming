c61bc9cfac65e0ba83c98d718bfc5a8adcc6a82a
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-13 22:12:47 +0000

    Corrections to code task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -10,8 +10,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) { // create an empty object
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
