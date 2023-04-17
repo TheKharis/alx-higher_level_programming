3bd1108255046c980eb0b6a1d620b5df9acebb37
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-17 02:13:44 +0000

    Initial upload of task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+# Get command line arguments
+mysql_user = sys.argv[1]
+mysql_pass = sys.argv[2]
+db_name = sys.argv[3]
+
+# Connect to MySQL server
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=mysql_username,
+    passwd=mysql_password,
+    db=database_name)
+
+# Create a cursor object to execute queries
+cursor = db.cursor()
+
+# Execute the query to get all states
+cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                    ORDER BY states.id""")
+
+# Fetch all rows and print them
+states = cursor.fetchall()
+for state in states:
+    print(state)
+
+# Close the database connection
+db.close()
+cursor.close()
