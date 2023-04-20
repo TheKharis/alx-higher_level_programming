2ae1c0d0cde569cbfe24819f9457eb56f1a1464f
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2023-04-20 23:39:34 +0000

    Fixing task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,28 +12,34 @@ import MySQLdb
 if __name__ == '__main__':
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
 
     # connect to database
     try:
-        db = MySQLdb.connect(host="localhost", user=mysql_username,
-                             passwd=mysql_password, db=database_name,
+        db = MySQLdb.connect(host="localhost",
+                             user=mysql_username,
+                             passwd=mysql_password,
+                             db=db_name,
                              port=3306)
         cur = db.cursor()
-        # Execute query search
-        query = """SELECT cities.id, cities.name, states.name FROM cities
-        JOIN states ON cities.state_id = states.id WHERE states.name = %s
-        ORDER BY cities.id ASC"""
-
-        cur.execute(query, (state_name,))
-
-        cities = cur.fetchall()
-
-        # print results
-        for city in cities:
-            print(city)
-
     except MySQLdb.Error as e:
-        print(f"MySQL Error {e.args[0]}: {e.args[1]}")
+        print("Error connecting to MySQL db: {}".format(e))
         sys.exit(1)
+
+    # execute query (make safe from sql injection)
+    try:
+        query = """SELECT cities.name FROM cities INNER JOIN states
+                ON states.id=cities.state_id WHERE states.name=%s
+                ORDER BY cities.id ASC"""
+        cur.execute(query, (state_name,))
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing MYSQL query: {}".format(e))
+        sys.exit(1)
+    # print results
+    hdisp = list(row[0] for row in rows)
+    print(*hdisp, sep=", ")
+    # clean up
+    cur.close()
+    db.close()
