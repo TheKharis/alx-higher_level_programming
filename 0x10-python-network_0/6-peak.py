7c2f3f0f1ec5e4573488e81116e050662fa26b9b
TheKharis <reubenagbenyegah86@gmail.com>
2023-04-28 05:54:52 +0000

    Fixing code, task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,13 +18,14 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    low, high = 0, len(list_of_integers) - 1
+    left = 0
+    right = len(list_of_integers) - 1
 
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
         else:
-            low = mid + 1
+            right = mid
 
-    return list_of_integers[low]
+    return list_of_integers[left]
