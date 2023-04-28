ca09c771375054f101cd7ce441bd484f9584e1da
TheKharis <reubenagbenyegah86@gmail.com>
2023-04-28 10:13:39 +0000

    Further corrections

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Makes a request that causes the server to respond with a message
-curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonShool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonShool" 0.0.0.0:5000/catch_me
