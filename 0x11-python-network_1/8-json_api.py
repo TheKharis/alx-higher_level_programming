04e1f2c34ce861ba1a2fe472fabd5acca400ac8a
TheKharis <reubenagbenyegah86@gmail.com>
2023-04-28 21:58:05 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = " "
+
+    url = "http://0.0.0.0:5000/search_user"
+    resp = requests.post(url, data={"q": q})
+
+    try:
+        json_resp = resp.json()
+    except ValueError:
+        print("Not a valid JSON")
+    if json_resp:
+        print("[{}] {}".format(json_resp["id"], json_resp["name"]))
+    else:
+        print("No result")
