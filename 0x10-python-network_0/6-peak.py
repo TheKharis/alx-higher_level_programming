d55f5fa2bc12b99e47d0155c62b46850e935988a
TheKharis <reubenagbenyegah86@gmail.com>
2023-04-28 09:45:53 +0000

    Advanced task 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""
-Find a peak in a list of unsorted integers.
-
-Args:
-    list_of_integers (list): list of integers to search for a peak.
-
-Returns:
-    int: a peak value in the list.
-
-Complexity:
-    O(log(n)) - Binary search algorithm.
-
-"""
-
-
 def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+    Args:
+        list_of_integers (list): list of integers to search for a peak.
+        Returns:
+            int: a peak value in the list.
+"""
+
     if not list_of_integers:
         return None
 
-    left = 0
-    right = len(list_of_integers) - 1
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
 
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    mid = int(n // 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
