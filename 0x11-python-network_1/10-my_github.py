dca4398c614c31a00d62277bc646d26d61632650
TheKharis <reubenagbenyegah86@gmail.com>
2023-09-29 22:58:39 +0000

    Correction of code, task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,34 +6,10 @@ uses the GitHub API to display your id
 
 import sys
 import requests
-
-
-def get_github_user_id(username: str, token: str) -> str:
-    """
-    Retrives the github user id.
-
-    Args:
-        username (str): The github username
-        token (str): The github personal access token
-
-    Retruns:
-        The github user id (str)
-        """
-
-    url = "https://api.github.com/user"
-    resp = requests.get(url, auth=(username, token))
-
-    if resp.status_code != 200:
-        sys.exit("Error: Could not retrieve user id")
-    return resp.json().get("id")
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("Usage: {} username token".format(sys.argv[0]))
-
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    github_user_id = get_github_user_id(username, token)
-    print(github_user_id)
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
