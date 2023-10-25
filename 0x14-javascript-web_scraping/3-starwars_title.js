abda00a5bc7d227faa2b93c06eaabc1836cfdeef
TheKharis <reubenagbenyegah86@gmail.com>
2023-10-25 05:21:49 +0000

    Tasks 5 - 6

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const id = process.argv[2];
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, res, body) => {
+  if (err) console.log(err);
+  console.log(body.title);
 });
